5b64cb75f23078740130640021ef6ede1eb9270b
Lance Kevin <lancekevin@Lances-MacBook-Air-2.local>
2019-10-26 18:07:36 -0400

    initial commit of the checked out item class

---
+++ src/library_system/Checked_out_itm.java
@@ -0,0 +1,52 @@
+package library_system;
+
+public class Checked_out_itm {
+	private String type;
+	private int renewals;
+	private int time_remaining;
+	private String title;
+	private int id;
+	
+	public Checked_out_itm(String t, Item item) {
+		type = t;
+		renewals = 0;
+		time_remaining = item.getMax_checkout_time();
+		title = item.getTitle();
+		id = item.getId();
+	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getRenewals() {
+		return renewals;
+	}
+	public void setRenewals(int renewals) {
+		this.renewals = renewals;
+	}
+	public int getTime_remaining() {
+		return time_remaining;
+	}
+	public void setTime_remaining(int time_remaining) {
+		this.time_remaining = time_remaining;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String name) {
+		this.title = name;
+	}
+	
+	
+}
