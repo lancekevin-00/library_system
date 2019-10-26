728d23a2e8e4acff0825b15c6ef91b523aa41bd1
Lance Kevin <lancekevin@Lances-MacBook-Air-2.local>
2019-10-26 18:06:48 -0400

    initial commit of the item class

---
+++ src/library_system/Item.java
@@ -0,0 +1,56 @@
+package library_system;
+
+public abstract class Item {
+	protected int max_checkout_time;
+	protected User[] waitlist;
+	protected int copies_avalible;
+	protected int copies_in_stock;
+	protected String[] searchTerms;
+	protected String title;
+	protected int id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMax_checkout_time() {
+		return max_checkout_time;
+	}
+	public void setMax_checkout_time(int max_checkout_time) {
+		this.max_checkout_time = max_checkout_time;
+	}
+	public User[] getWaitlist() {
+		return waitlist;
+	}
+	public void setWaitlist(User[] waitlist) {
+		this.waitlist = waitlist;
+	}
+	public int getCopies_avalible() {
+		return copies_avalible;
+	}
+	public void setCopies_avalible(int copies_avalible) {
+		this.copies_avalible = copies_avalible;
+	}
+	public int getCopies_in_stock() {
+		return copies_in_stock;
+	}
+	public void setCopies_in_stock(int copies_in_stock) {
+		this.copies_in_stock = copies_in_stock;
+	}
+	public String[] getSearchTerms() {
+		return searchTerms;
+	}
+	public void setSearchTerms(String[] searchTerms) {
+		this.searchTerms = searchTerms;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
