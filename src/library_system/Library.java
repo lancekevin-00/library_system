9547b3a66f4c112e65dd87bb20d2824459ced472
Lance Kevin <lancekevin@Lances-MacBook-Air-2.local>
2019-11-03 16:06:42 -0500

    multiple users can use the system without needing to re-run

--- src/library_system/Library.java
+++ src/library_system/Library.java
@@ -12,183 +12,195 @@ public class Library {
 		ArrayList<User> Users = UserLoader.loadUsers();
 		Item_DB stock = new Item_DB();
 		User curr_user = null;
-		
+		boolean total_logout = false;
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("~~~~~~~~~~~~~~WELCOME TO THE LIBRARY~~~~~~~~~~~~~~");
 		do {
-			System.out.println("enter your card # to login: ");
-			int card_num = scan.nextInt();	
-	
-			//login functionality for when the user loader is functioning
-			System.out.println("Searching for the number " + card_num + " in the user database");
-			boolean found = false;
-			for(User user: Users) {
-				if(user.getCardNumber() == card_num) {
-					found = true;
-					int pwd_attmpts = 3;
-					String pwd;
-					do {
-						System.out.println("please enter your password to continue: ");
-						pwd = scan.next();
-						System.out.println("checking if the password " + pwd + " is correct");
-						if (user.getPassword().equals(pwd)){
-							System.out.println("login successful");
-							curr_user = user;
-						}
-						else {
-							System.out.println("incorrect password " + pwd_attmpts + " attempts remaining");
-							pwd_attmpts--;
-						}
-					}
-					while(pwd_attmpts > 0 && curr_user == null);
-				}
-			}
-			if (!found) {
-				System.out.println("that card number does not exist");
-			}
-			else {
-				System.out.println("you ran out of attempts");
-			}
-			
-		}
-		while(curr_user == null);
-
-
-		//this is an example user in the system used for testing the menu
-		//curr_user = new Adult(001, "Example", "Name", 2000, "1234 Example lane" , 001, " Password", 0.00, new Child[0]);
+			System.out.println("~~~~~~~~~~~~~~WELCOME TO THE LIBRARY~~~~~~~~~~~~~~");
+			do {
+				System.out.println("enter your card # to login: ");
+				int card_num = scan.nextInt();	
 		
-		System.out.println("Welcome to the library " + curr_user.getName());
+				//login
+				System.out.println("Searching for the number " + card_num + " in the user database");
+				boolean found = false;
+				for(User user: Users) {
+					if(user.getCardNumber() == card_num) {
+						found = true;
+						int pwd_attmpts = 3;
+						String pwd;
+						do {
+							System.out.println("please enter your password to continue: ");
+							pwd = scan.next();
+							System.out.println("checking if the password " + pwd + " is correct");
+							if (user.getPassword().equals(pwd)){
+								System.out.println("login successful");
+								curr_user = user;
+							}
+							else {
+								System.out.println("incorrect password " + pwd_attmpts + " attempts remaining");
+								pwd_attmpts--;
+							}
+						}
+						while(pwd_attmpts > 0 && curr_user == null);
+					}
+				}
+				if (!found) {
+					System.out.println("that card number does not exist");
+				}
+				else {
+					System.out.println("you ran out of attempts");
+				}
 				
-		try {
-			boolean go_again = true;
-			while(go_again) {
-				System.out.println("~~~~~~~~~~~~~~MENU~~~~~~~~~~~~~~");
-				System.out.println("1: Search");
-				System.out.println("2: Check Out");
-				System.out.println("3: Check Due Dates");
-				System.out.println("4: Return");
-				System.out.println("5: Check Fines");
-				System.out.println("6: Pay Fines");
-				System.out.println("7: Change Email Address");
-				System.out.println("8: Change Password");
-				System.out.println("9: Log out");
-				if(curr_user.is_librarian)
-					System.out.println("10: Edit User");
-
-				int choice = scan.nextInt();
-				scan.nextLine();
-				switch(choice) {
-				case 1:
-					System.out.println("Enter a search term: ");
-					String term = scan.nextLine();
-					System.out.println(term);
-					//Create a book arraylist
-					ArrayList<Book> bookStock = stock.getBooks();
-					//Create a DVD arraylist
-					ArrayList<DVD> dvdStock = stock.getDVDs();
-					//Create a Magazine arraylist
-					ArrayList<Magazine> magazineStock = stock.getMagazines();
-					ArrayList<eBook> ebookStock = stock.geteBooks();
-					ArrayList<Audio_Book> audiobookStock = stock.getaudioBooks();
-					System.out.println("Searching through the stock database for the search term");
-					System.out.println("Results:");
-					//All this subject to change once we get them all into one arraylist 
-					//Search through the books search terms and print id and title fo the book(can add other stuff later
-					for(Book book : bookStock) {
-						for(int i = 0; i < 3; i++) {
-							if(book.getSearchTerms(i).contains(term)) {
-								System.out.println(book.getId() + ": " + book.getTitle());
-								i = 4;
-							}		
-						}
-					} 
-					//Search throught the dvd search terms
-					for(DVD dvd : dvdStock) {
-						for(int j = 0; j < 3; j++) {
-							if(dvd.getSearchTerms(j).contains(term)) {
-								System.out.println(dvd.getId() + ": " + dvd.getTitle());
-								j = 4;
-							}		
-						}
-					}
-					//Search through the magazine search terms
-					for(Magazine magazine : magazineStock) {
-						for(int i = 0; i < 3; i++) {
-							if(magazine.getSearchTerms(i).contains(term)) {
-								System.out.println(magazine.getId() + ": " + magazine.getTitle());
-								i = 4;
-							}		
-						}
-					}
-					//Search through the eBook stock
-					for(eBook ebook : ebookStock) {
-						for(int i = 0; i < 3; i++) {
-							if(ebook.getSearchTerms(i).contains(term)) {
-								System.out.println(ebook.getId() + ": " + ebook.getTitle());
-								i = 4;
-							}		
-						}
-					}
-					//Serach through the audioBook search terms
-					for(Audio_Book audiobook : audiobookStock) {
-						for(int i = 0; i < 3; i++) {
-							if(audiobook.getSearchTerms(i).contains(term)) {
-								System.out.println(audiobook.getId() + ": " + audiobook.getTitle());
-								i = 4;
-							}		
-						}
-					}
+			}
+			while(curr_user == null);
+	
+	
+			//this is an example user in the system used for testing the menu
+			//curr_user = new Adult(001, "Example", "Name", 2000, "1234 Example lane" , 001, " Password", 0.00, new Child[0]);
+			
+			System.out.println("Welcome to the library " + curr_user.getName());
 					
-					System.out.println("Select a Result by id# or enter zero to exit:");
-					int id = scan.nextInt();
-					if(id == 0)
+			try {
+				boolean go_again = true;
+				while(go_again) {
+					System.out.println("~~~~~~~~~~~~~~MENU~~~~~~~~~~~~~~");
+					System.out.println("1: Search");
+					System.out.println("2: Check Out");
+					System.out.println("3: Check Due Dates");
+					System.out.println("4: Return");
+					System.out.println("5: Check Fines");
+					System.out.println("6: Pay Fines");
+					System.out.println("7: Change Email Address");
+					System.out.println("8: Change Password");
+					System.out.println("9: Log out");
+					if(curr_user.is_librarian)
+						System.out.println("10: Edit User");
+	
+					int choice = scan.nextInt();
+					scan.nextLine();
+					switch(choice) {
+					case 1:
+						System.out.println("Enter a search term: ");
+						String term = scan.nextLine();
+						System.out.println(term);
+						//Create a book arraylist
+						ArrayList<Book> bookStock = stock.getBooks();
+						//Create a DVD arraylist
+						ArrayList<DVD> dvdStock = stock.getDVDs();
+						//Create a Magazine arraylist
+						ArrayList<Magazine> magazineStock = stock.getMagazines();
+						ArrayList<eBook> ebookStock = stock.geteBooks();
+						ArrayList<Audio_Book> audiobookStock = stock.getaudioBooks();
+						System.out.println("Searching through the stock database for the search term");
+						System.out.println("Results:");
+						//All this subject to change once we get them all into one arraylist 
+						//Search through the books search terms and print id and title fo the book(can add other stuff later
+						for(Book book : bookStock) {
+							for(int i = 0; i < 3; i++) {
+								if(book.getSearchTerms(i).contains(term)) {
+									System.out.println(book.getId() + ": " + book.getTitle());
+									i = 4;
+								}		
+							}
+						} 
+						//Search throught the dvd search terms
+						for(DVD dvd : dvdStock) {
+							for(int j = 0; j < 3; j++) {
+								if(dvd.getSearchTerms(j).contains(term)) {
+									System.out.println(dvd.getId() + ": " + dvd.getTitle());
+									j = 4;
+								}		
+							}
+						}
+						//Search through the magazine search terms
+						for(Magazine magazine : magazineStock) {
+							for(int i = 0; i < 3; i++) {
+								if(magazine.getSearchTerms(i).contains(term)) {
+									System.out.println(magazine.getId() + ": " + magazine.getTitle());
+									i = 4;
+								}		
+							}
+						}
+						//Search through the eBook stock
+						for(eBook ebook : ebookStock) {
+							for(int i = 0; i < 3; i++) {
+								if(ebook.getSearchTerms(i).contains(term)) {
+									System.out.println(ebook.getId() + ": " + ebook.getTitle());
+									i = 4;
+								}		
+							}
+						}
+						//Serach through the audioBook search terms
+						for(Audio_Book audiobook : audiobookStock) {
+							for(int i = 0; i < 3; i++) {
+								if(audiobook.getSearchTerms(i).contains(term)) {
+									System.out.println(audiobook.getId() + ": " + audiobook.getTitle());
+									i = 4;
+								}		
+							}
+						}
+						
+						System.out.println("Select a Result by id# or enter zero to exit:");
+						int id = scan.nextInt();
+						if(id == 0)
+							break;
+						
+						System.out.println("~~~~~~~~~ACTIONS~~~~~~~~~");
+						System.out.println("1: Get Info");
+						System.out.println("2: Checkout");
+						System.out.println("3: Return to Menu");
+						int c = scan.nextInt();
+						boolean i = true;
+						//while(i) {
+						switch(c) {
+							case 1:
+								System.out.println("Getting Book Info");
+								
+								break;
+							case 2:
+								System.out.println("Checkout The Item");
+								break;
+							case 3:
+								System.out.println("Return to Menu");
+								i = false;
+								break;
+							}
+						//}
 						break;
-					
-					System.out.println("~~~~~~~~~ACTIONS~~~~~~~~~");
-					System.out.println("1: Get Info");
-					System.out.println("2: Checkout");
-					System.out.println("3: Return to Menu");
-					int c = scan.nextInt();
-					boolean i = true;
-					//while(i) {
-					switch(c) {
+					case 2: //I think we should get rid of this and just leave checkout to the search action
+						System.out.println("Checking Out");
+						break;
+					case 3:
+						System.out.println("Check Due Dates");
+						Checked_out_itm[] items = curr_user.getItems();
+						for(Checked_out_itm item: items) {
+							System.out.println(item.getTitle()+": "+item.getTime_remaining()+" days remaining");
+						}
+						break;
+					case 4:
+						System.out.println("Return");
+						//select a book or books which you are returning
+						break;
+					case 5:
+						System.out.println("You owe: $" + curr_user.getFees());
+						System.out.println("~~~~~~~~~ACTIONS~~~~~~~~~");
+						System.out.println("1: Make a payment");
+						System.out.println("2: Return to Menu");
+						int n = scan.nextInt();
+						switch(n) {
 						case 1:
-							System.out.println("Getting Book Info");
-							
+							System.out.println("Enter payment amount");
+							System.out.print("$");
+							int amt = scan.nextInt();
+							curr_user.makePayment(amt);
+							System.out.println("Thank you for your payment");
+							System.out.println("You now owe "+ curr_user.getFees() + " in fines");
 							break;
 						case 2:
-							System.out.println("Checkout The Item");
 							break;
-						case 3:
-							System.out.println("Return to Menu");
-							i = false;
-							break;
-						}
-					//}
-					break;
-				case 2: //I think we should get rid of this and just leave checkout to the search action
-					System.out.println("Checking Out");
-					break;
-				case 3:
-					System.out.println("Check Due Dates");
-					Checked_out_itm[] items = curr_user.getItems();
-					for(Checked_out_itm item: items) {
-						System.out.println(item.getTitle()+": "+item.getTime_remaining()+" days remaining");
-					}
-					break;
-				case 4:
-					System.out.println("Return");
-					//select a book or books which you are returning
-					break;
-				case 5:
-					System.out.println("You owe: $" + curr_user.getFees());
-					System.out.println("~~~~~~~~~ACTIONS~~~~~~~~~");
-					System.out.println("1: Make a payment");
-					System.out.println("2: Return to Menu");
-					int n = scan.nextInt();
-					switch(n) {
-					case 1:
+						};
+						break;
+					case 6:
 						System.out.println("Enter payment amount");
 						System.out.print("$");
 						int amt = scan.nextInt();
@@ -196,57 +208,51 @@ public class Library {
 						System.out.println("Thank you for your payment");
 						System.out.println("You now owe "+ curr_user.getFees() + " in fines");
 						break;
-					case 2:
+					case 7:
+						System.out.println("Enter your new email address: ");
+						String new_email = scan.next();
+						//check for validity of new email
+						//set users email to the new email
 						break;
-					};
-					break;
-				case 6:
-					System.out.println("Enter payment amount");
-					System.out.print("$");
-					int amt = scan.nextInt();
-					curr_user.makePayment(amt);
-					System.out.println("Thank you for your payment");
-					System.out.println("You now owe "+ curr_user.getFees() + " in fines");
-					break;
-				case 7:
-					System.out.println("Enter your new email address: ");
-					String new_email = scan.next();
-					//check for validity of new email
-					//set users email to the new email
-					break;
-				case 8:
-					System.out.println("Enter your new password:");
-					String pwd1 = scan.next();
-					System.out.println("Confirm your password:");
-					String pwd2 = scan.next();
-					if(pwd1.equals(pwd2)) {
-						System.out.println("new password confirmed");
-						curr_user.changePwd(pwd2);
+					case 8:
+						System.out.println("Enter your new password:");
+						String pwd1 = scan.next();
+						System.out.println("Confirm your password:");
+						String pwd2 = scan.next();
+						if(pwd1.equals(pwd2)) {
+							System.out.println("new password confirmed");
+							curr_user.changePwd(pwd2);
+						}
+						else {
+							System.out.println("passwords do not match");
+							System.out.println("new password not confirmed");
+						}
+						break;
+					case 9:
+						go_again = false;
+						curr_user = null;
+						System.out.println("Good bye" + curr_user.Name);
+						break;
+					case 10:
+						if(curr_user.is_librarian)
+					    System.out.println("Entering the edit user menu");
+						else
+						System.out.println("Enter A Valid Number");
+						break;
+					case 11:
+						System.out.println("EXITING THE SYSTEM");
+						total_logout = true;
+					default:
+						System.out.println("Enter A Valid Number");
+						break;
 					}
-					else {
-						System.out.println("passwords do not match");
-						System.out.println("new password not confirmed");
-					}
-					break;
-				case 9:
-					go_again = false;
-					System.out.println("Good bye");
-					break;
-				case 10:
-					if(curr_user.is_librarian)
-				    System.out.println("Entering the edit user menu");
-					else
-					System.out.println("Enter A Valid Number");
-					break;
-				default:
-					System.out.println("Enter A Valid Number");
-					break;
 				}
 			}
-			scan.close();
+			catch (Exception e) {
+				System.out.println(e);
+			}
 		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+		while(!total_logout);
+		scan.close();
 	}
 }
