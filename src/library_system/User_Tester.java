8485a01845c21f4ed7b4fcec635e5ae2a03719c0
Lance Kevin <lancekevin@Lances-MacBook-Air-2.local>
2019-11-17 13:34:49 -0500

    got the configurations correct and test 1 to work

--- src/library_system/User_Tester.java
+++ src/library_system/User_Tester.java
@@ -1,6 +1,6 @@
 package library_system;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -9,7 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-class User_Tester {
+public class User_Tester {
 	
 	User adult, teacher, librarian;
 	private final ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -20,7 +20,7 @@ class User_Tester {
 	}
 	
 	@Before
-	public void init_user() {
+	public void init() {
 		adult = new Adult(0, "John", "Doe", 2000, "1234 Example st", 0, "password" , 0, "A");
 		teacher = new Teacher(1, "School", "Teacher", 1980, "1 school street", 1, "password", 0, "T");
 		librarian = new Librarian(2, "the", "librarian", 0, "1 learning lane", 2, "password", 0, "L");
